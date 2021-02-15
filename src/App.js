039b4820def5c02b6f421edbb93c648b7e289d8e
PrajithSai <sai.prajith@gmail.com>
2021-02-15 12:54:24 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Tree from 'react-d3-tree';
+// import Tree from 'react-tree-graph';
+import { Header, Button } from 'semantic-ui-react'
+import Select from 'react-select'
+import { findIndex } from 'lodash'
+import 'semantic-ui-css/semantic.min.css'
+import { users } from './data/users'
+import './App.scss'
 
 function App() {
+  const [lastNodeNumber, incrementLastNodeNumber] = useState(18)
+  const [nodes, setNodes] = useState(users)
+  const [caller, saveCaller] = useState({ label: "", value: "" })
+  const [callee, saveCallee] = useState({ label: "", value: "" })
+  const flattenedNodes = flatten([nodes])
+
+  const straightPathFunc = (linkDatum, orientation) => {
+    const { source, target } = linkDatum;
+    return orientation === 'horizontal'
+      ? `M${source.y},${source.x}L${target.y},${target.x}`
+      : `M${source.x},${source.y}L${target.x},${target.y}`;
+  };
+
+  const handleNodeClick = (param) => {
+    console.log(param)
+  }
+
+  const getNodeOptions = () => {
+    const options = [];
+    for (let i = 0; i <= lastNodeNumber; i += 1) {
+      options.push({
+        label: i,
+        value: i,
+      })
+    }
+    return options
+  }
+
+function flatten (data, parentId = "0"){
+  return data.reduce((r, { children, name}) => {
+    r.push({name, parentId});
+    if (children) r.push(...flatten(children, name));
+    return r;
+  }, [])
+}
+
+  const findPath = (name, currentPath = []) => {
+    const index = findIndex(flattenedNodes, { name })
+    const currentNode = flattenedNodes[index]
+    if (currentNode.parentId !== "0") {
+      currentPath.push(currentNode.name)
+      return findPath(currentNode.parentId, currentPath)
+    } else {
+      currentPath.push(currentNode.name)
+      currentPath.push("0")
+      return currentPath;
+    }
+  }
+
+  const getInitialPath = () => {
+    const callerPath = findPath(String(caller.value))
+    const callePath = findPath(String(callee.value)).reverse()
+    console.log(`${callerPath.join('->')}->${callePath.slice(1).join('->')}`)
+  }
+
+  const setCaller = (option) => {
+    saveCaller(option)
+  }
+
+  const setCallee = (option) => {
+    saveCallee(option)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-div" style={{ margin: '15px', padding: '15px' }}>
+        {/* <div>
+          <Header as="h3">Add New Node</Header>
+        </div> */}
+        <div>
+          <Header as="h3">Caching</Header>
+          <div className="select-cache">
+            <label>Caller</label>
+            <Select onChange={setCaller} options={getNodeOptions()} />
+          </div>
+          <div className="select-cache">
+            <label>Callee</label>
+            <Select onChange={setCallee} options={getNodeOptions()} />
+          </div>
+          <div className="select-cache cache-buttons">
+            <Button primary onClick={getInitialPath}>Show Initial Path</Button>
+            <Button secondary>Show Cached Path</Button>
+          </div>
+        </div>
+        <div id="treeWrapper" style={{ width: '100em', height: '100em' }}>
+          <Tree data={nodes} orientation="vertical" pathFunc={straightPathFunc} onNodeClick={handleNodeClick} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data/users.js
@@ -0,0 +1,101 @@
+export const users = {
+  "name": "0",
+  "children": [{
+    "name": "1",
+    "children": [{
+      "name": "3",
+      "children": [{
+        "name": "7"
+      },{
+        "name": "8"
+      },{
+        "name": "9"
+      }]
+  }, {
+    "name": "4",
+    "children": [{
+      "name": "10"
+    },{
+      "name": "11"
+    },{
+      "name": "12"
+    }]
+}]
+},{
+  "name": "2",
+  "children": [{
+    "name": "5",
+    "children": [{
+      "name": "13"
+    },{
+      "name": "14"
+    },{
+      "name": "15"
+    }]
+},{
+  "name": "6",
+  "children": [{
+    "name": "16"
+  },{
+    "name": "17"
+  },{
+    "name": "18"
+  }]
+}]
+}]
+}
+
+
+// export const users = {
+//   "name": "0",
+//   "parentId": null,
+//   "children": [{
+//     "name": "1",
+//     "parentId": "0",
+//     "children": [{
+//       "name": "3",
+//       "parentId": "1",
+//       "children": [{
+//         "name": "7",
+//         "parentId": "3",
+//       },{
+//         "name": "8",
+//         "parentId": "3",
+//       }]
+//   }, {
+//     "name": "4",
+//     "parentId": "1",
+//     "children": [{
+//       "name": "9",
+//       "parentId": "4",
+//     },{
+//       "name": "10",
+//       "parentId": "4",
+//     }]
+// }]
+// },{
+//   "name": "2",
+//   "parentId": "0",
+//   "children": [{
+//     "name": "5",
+//     "parentId": "2",
+//     "children": [{
+//       "name": "11",
+//       "parentId": "5",
+//     },{
+//       "name": "12",
+//       "parentId": "5",
+//     }]
+// },{
+//   "name": "6",
+//   "parentId": "2",
+//   "children": [{
+//     "name": "13",
+//     "parentId": "6",
+//   },{
+//     "name": "14",
+//     "parentId": "6",
+//   }]
+// }]
+// }]
+// }
